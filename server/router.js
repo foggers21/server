9d89a0bc9d22b1f047985160824750f99f1ac857
foggers21 <vanya21254321@gmail.com>
2019-02-16 12:56:15 +0200

    fix8

--- server/router.js
+++ server/router.js
@@ -26,7 +26,7 @@ function createToken (body) {
     );
 }
 
-const passport = require('passport');
+
 const { Strategy } = require('passport-jwt');
 
 passport.use(new Strategy(jwt, function(jwt_payload, done) {
